9e1b068e17ab48d2773a07437e48b74eae4f0f29
Roman Yephimov <roman.yephimov@litslink.com>
2019-10-02 09:36:56 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    isActive: false,
+    path: ''
+  };
+
+  onPathChange = event => {
+      this.setState({ path: event.target.value })
+  };
+
+  changeSwitcher = () => this.setState({ isActive: !this.state.isActive });
+
+  render() {
+      return (
+        <div className="App">
+          {/* @TODO: make like className */}
+          <div>{this.state.isActive ? 'On' : 'Off'}</div>
+          <div style={{ width: 25, height: 25, borderRadius: 20, background: this.state.isActive ? 'green' : 'gray' }}></div>
+            <button onClick={this.changeSwitcher}>On / Off </button>
+            <img src={this.state.path} alt="" width="100" height="100" />
+            <input value={this.state.path} onChange={this.onPathChange} type="text" />
+            <MyComp name="Alex">
+
+            </MyComp>
+        </div>
+    );
+  }
+}
+
+
+const MyComp = props => {
+    return (
+        <>
+            <div>{props.name}</div>
+            <div>
+                {props.children}
+            </div>
+        </>
+    )
 }
 
 export default App;
